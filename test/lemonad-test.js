cb9bd4e96ab6779ed8bdf43103c4efaa6f6ea134
Fogus <mefogus@gmail.com>
2012-12-14 15:35:31 -0500

    more vows

--- test/lemonad-test.js
+++ test/lemonad-test.js
@@ -3,15 +3,26 @@ var vows = require('vows'),
     L = require('../lib/lemonad');
 
 vows.describe('iterateUntil').addBatch({
-    'with a function of zero args': {
+    'with a function of zero args and no seed': {
         topic: function() {
             return L.iterateUntil(function() {
-              return Math.floor(Math.random()*11);
-            },
-            function(n) { return n !== 0 });
+                                    return Math.floor(Math.random()*11);
+                                  },
+                                  function(n) { return n !== 0 });
         },
         'just check if it works at all': function(topic) {
             assert.isArray(topic);
+        }
+    },
+    'with a function of one arg': {
+        topic: function() {
+            return L.iterateUntil(function(n) { return n-1 },
+                                  function(n) { return n !== 0 },
+                                  10);
+        },
+        'just check if it gives us the array we expect': function(topic) {
+            assert.isArray(topic);
+            assert.deepEqual(topic, [9, 8, 7, 6, 5, 4, 3, 2, 1]);
         }
     }
 }).export(module);
