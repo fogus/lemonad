6e4b22694e94bbd4f6a0c1cdeb546ad6be1f4a03
fogus <mefogus@gmail.com>
2013-01-02 22:41:14 -0500

    HoF and Curry specs

---
+++ test/curry-specs.js
@@ -0,0 +1,24 @@
+describe("Currying functions", function() {
+  beforeEach(function() {
+
+  });
+
+  describe("curry2", function() {
+    it("should return a function curried for two args", function() {
+      var div10 = L.curry2(function(n, d) { return n / d; })(10);
+
+      expect(div10(50)).toEqual(5);
+    });
+  });
+
+  describe("curry3", function() {
+    it("should return a function curried for three args", function() {
+      var hexColor = function(r, g, b) { return [r,g,b].join(''); };
+      var blue = L.curry3(hexColor)('FF');
+      var blueGreen = L.curry3(hexColor)('FF')('EE');
+
+      expect(blue('01')('00')).toEqual('0001FF');
+      expect(blueGreen('00')).toEqual('00EEFF');
+    });
+  });
+});
--- test/hof-specs.js
+++ test/hof-specs.js
@@ -10,4 +10,29 @@ describe("Higher-order functions", function() {
       expect(L.best(function(x,y) { return x > y; }, a)).toEqual(5);
     });
   });
+
+  describe("complement", function() {
+    var _e = L.complement(L.isOdd);
+    var _o = L.complement(L.isEven);
+
+    it("should return a predicate that gives the opposite result of a given predicate", function() {
+      expect(_e(4)).toBeTruthy();
+      expect(_e(0)).toBeTruthy();
+      expect(_e(-4)).toBeTruthy();
+      expect(_o(5)).toBeTruthy();
+      expect(_o(1)).toBeTruthy();
+      expect(_o(0)).toBeFalsy();
+    });
+  });
+
+  describe("fnull", function() {
+    it("should return a null-safe function with default args filled in", function() {
+      var a = [1,2,3,null,5];
+      var b = [1,2,3,undefined,5];
+      var safeMult = L.fnull(function(total, n) { return total * n; }, 1, 1);
+
+      expect(_.reduce(a, safeMult)).toEqual(30);
+      expect(_.reduce(b, safeMult)).toEqual(30);
+    });
+  });
 });
