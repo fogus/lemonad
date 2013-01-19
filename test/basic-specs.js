f075710fda027254136a226cc65ac12b0e3b655c
fogus <mefogus@gmail.com>
2013-01-19 10:26:59 -0500

    added ctor

--- test/basic-specs.js
+++ test/basic-specs.js
@@ -3,6 +3,16 @@ describe("Basic functions", function() {
 
   });
 
+  describe("ctor", function() {
+    it("should return the constructor of the object", function() {
+      var O = function() { this.foo = 42; };
+
+      expect(L.ctor([])).toBe(Array);
+      expect(L.ctor({})).toBe(Object);
+      expect(L.ctor(new O())).toBe(O);
+    });
+  });
+
   describe("cat", function() {
     it("should concatenate two arrays", function() {
       var a = [1,2,3];
