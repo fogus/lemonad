4b31d9f049f25ae5165e6555f11c7dc23d2a33e3
Fogus <mefogus@gmail.com>
2013-04-30 12:04:10 -0400

    Removed flip2/partial$

--- test/combinator-specs.js
+++ test/combinator-specs.js
@@ -1,10 +1,2 @@
 describe("Combinators", function() {
-  describe("flip2", function() {
-    it("should return a function that applies a given function with its first two args flipped", function() {
-      var div = function(n, d) { return n/d; };
-
-      expect(div(10,2)).toEqual(5);
-      expect(L.flip2(div)(10,2)).toEqual(0.2);
-    });
-  });
 });
