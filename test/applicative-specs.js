2d5259699aecb5d1d86f704188142b29b6aac5fc
Fogus <mefogus@gmail.com>
2013-04-30 11:58:41 -0400

    Removed juxt/iterateUntil

--- test/applicative-specs.js
+++ test/applicative-specs.js
@@ -18,21 +18,6 @@ describe("Applicative functions", function() {
     });
   });
 
-  describe("juxt", function() {
-    it("should return a function that returns an array of the originally supplied functions called", function() {
-      var f = L.juxt(_.isPositive, _.isEven, _.isZero);
-
-      expect(f(1)).toEqual([true, false, false]);
-      expect(f()).toEqual([false, false, false]);
-    });
-  });
-
-  describe("iterateUntil", function() {
-    it("should call a function recursively f(f(f(args))) until a given function goes falsey", function() {
-      expect(L.iterateUntil(function(n){ return n-1; }, _.isPositive, 6)).toEqual([5,4,3,2,1]);
-    });
-  });
-
   describe("reductions", function() {
     it("should perform a reduce and collect the intermediate results in an array", function() {
       expect(L.reductions(function(agg, n){ return agg+n; }, 0, [1,2,3,4,5])).toEqual([1,3,6,10,15]);
