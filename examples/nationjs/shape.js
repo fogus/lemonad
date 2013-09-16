7278cc0e9694891cd6638c51b77a0cdf59f2d331
fogus <mefogus@gmail.com>
2013-09-15 22:40:57 -0400

    Adding NationJS examples

---
+++ examples/nationjs/shape.js
@@ -0,0 +1,20 @@
+var itunes = [
+    {'artist' : 'Sun Ra',
+     'genre'  : 'Jazz',
+     'plays'  : 379},
+
+    {'artist' : 'Om',
+     'genre'  : 'Doom',
+     'plays'  : 22},
+
+    {'artist' : 'Can',
+     'genre'  : 'Prog',
+     'plays'  : 190},
+
+    {'artist' : 'Popol Vuh',
+     'genre'  : 'Prog',
+     'plays'  : 285}
+];
+
+    
+               
