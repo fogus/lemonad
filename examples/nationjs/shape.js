56c7780e32bcb3f68a364b51a915d1936ec8721a
fogus <mefogus@gmail.com>
2013-09-15 23:04:22 -0400

    Adding NationJS examples

--- examples/nationjs/shape.js
+++ examples/nationjs/shape.js
@@ -16,5 +16,25 @@ var itunes = [
      'plays'  : 285}
 ];
 
-    
-               
+function cell(col, row, table) {
+  var entry = table[row];
+  return entry && entry[col];
+}
+
+cell('artist',
+     /* of row */ 1, 
+     /* from */   itunes);
+//=> "Om"
+
+function getArtists(library) {
+  var ret = [];
+
+  for (var i = 0; i < library.length; i++) {
+    ret.push(cell('artist', i, library));
+  }
+
+  return ret;
+}
+
+getArtists(itunes);
+//=> ["Sun Ra", "Om", "Can", "Popol Vuh"]
