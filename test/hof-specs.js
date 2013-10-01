b9d4e0e2dbc717576a67b532b405599b17c72b4a
fogus <mefogus@gmail.com>
2013-10-01 08:26:11 -0400

    Fixed L.rot to be more Forth-like

--- test/hof-specs.js
+++ test/hof-specs.js
@@ -3,9 +3,9 @@ describe("Higher-order functions", function() {
       it("should rotate arguments applied", function() {
         var identity = function() { return [].slice.call(arguments); };
         var rotate = L.rot(identity)(1, 2, 3);
-        expect(rotate[0]).toBe(2);
-        expect(rotate[1]).toBe(3);
-        expect(rotate[2]).toBe(1);
+        expect(rotate[0]).toBe(3);
+        expect(rotate[1]).toBe(1);
+        expect(rotate[2]).toBe(2);
       });
     });
 });
