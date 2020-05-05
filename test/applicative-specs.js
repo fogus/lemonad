3534077fbecd8e8618f4597fc3c5c6bdaca7d52a
Fogus <mefogus@gmail.com>
2020-05-05 08:40:05 -0400

    Migrating to Jasmine

--- test/applicative-specs.js
+++
@@ -1,3 +0,0 @@
-describe("Applicative functions", function() {
-
-});
---
+++ test/specs/applicative-specs.js
@@ -0,0 +1,7 @@
+var Immutable = require('immutable');
+
+describe("Applicative functions", function() {
+    var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+
+    expect(map1.get('b')).toBe(2);
+});
