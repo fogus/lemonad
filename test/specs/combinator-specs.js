b629f23b6f223adacd7c383f371f4fa22aeb43a8
Fogus <mefogus@gmail.com>
2020-05-07 10:39:09 -0400

    The great transition starts

--- test/specs/combinator-specs.js
+++ test/specs/combinator-specs.js
@@ -1,2 +1,7 @@
 describe("Combinators", function() {
+  it("TODO placeholder", function() {
+    var map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+    
+    expect(map1.get('b')).toBe(2);
+  });
 });
